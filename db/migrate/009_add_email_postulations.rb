ab5c93e505316d6d85702fa9ad91b9151ea8ee7a
pdpire <pablopire1@gmail.com>
2015-11-15 15:35:28 -0300

    add migrate to postulations

---
+++ db/migrate/009_add_email_postulations.rb
@@ -0,0 +1,13 @@
+migration 9, :add_email_postulations do
+  up do
+    modify_table :job_applications do
+      add_column :email, String
+    end
+  end
+
+  down do
+    modify_table :job_applications do
+      drop_column :email
+    end
+  end
+end
