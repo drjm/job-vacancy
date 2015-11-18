2232da6a5811cd2c3bbd74c3267596be7228b9eb
David Galeano <degnexo@gmail.com>
2015-11-18 02:40:04 -0300

    commit with the scenario 'User access to their profile and edit their data, but cancel it' of the User story 'as a user i want to edit my data info' passing

--- features/as_a_user_i_want_to_edit_my_data_info.feature
+++ features/as_a_user_i_want_to_edit_my_data_info.feature
@@ -2,15 +2,15 @@ Feature: as a user i want to edit my data info.
 
 Background: User has logged in.
   Given a registration page
-  And put the name "Carlos"
-  And put the email "Carlos@gmail.com"
+  And put the name "Carlos R"
+  And put the email "Carlos_r@gmail.com"
   And put the password "CarlOs93"
   And put the password confirm "CarlOs93"
   And put the specialties "PHP programmer"
   And end the registration
   And visit the homepage
-  And log in as "Carlos@gmail.com"
-  When access to the profile page
+  And log in as "Carlos_r@gmail.com"
+  When access to the profile
   Then access to edit page
 
 
@@ -26,5 +26,9 @@ Scenario: User access to their profile and edit their name
   When edit their name "Carlos Rodríguez"
   Then access to their profile seeing their name modified "Carlos Rodríguez"
 
+
+Scenario: User access to their profile and edit their data, but cancel it.
+  When edit their email "Carlos_93@gmail.com" and cancel the changes
+  Then access to their profile seeing email with "Carlos_r@gmail.com"
 
 
--- features/step_definitions/as_a_user_i_want_to_edit_my_data_info_steps.rb
+++ features/step_definitions/as_a_user_i_want_to_edit_my_data_info_steps.rb
@@ -3,6 +3,11 @@ When(/^edit their email with "(.*?)"$/) do |arg1|
   click_button('Accept')
 end
 
+When(/^access to the profile$/) do
+  click_link 'Carlos_r@gmail.com'
+end
+
+
 Then(/^access to their profile seeing their email modified with "(.*?)"$/) do |arg1|
   page.should have_content(arg1)
 end
@@ -21,15 +26,17 @@ Then(/^access to their profile seeing their name modified "(.*?)"$/) do |arg1|
 end
 
 When(/^edit their password with "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  fill_in('Password:', :with => arg1)
+  fill_in('Password confirmation:', :with => arg1)
+  click_button('Accept')
 end
 
 Then(/^access to their profile seeing their password modified with "(.*?)"\.$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  page.should have_content(arg1)
 end
 
 Then(/^access to their profile seeing password with "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  page.should have_content(arg1)
 end
 
 When(/^edit their specialties with "(.*?)"$/) do |arg1|
@@ -38,13 +45,17 @@ When(/^edit their specialties with "(.*?)"$/) do |arg1|
 end
 
 When(/^edit their email "(.*?)" and cancel the changes$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  fill_in('Email:', :with => arg1)
+  visit '/profile'
 end
 
 When(/^access to edit page$/) do
   visit "/edit"
 end
 
+Then(/^access to their profile seeing email with "(.*?)"$/) do |arg1|
+  page.should have_content(arg1)
+end
 
 
 
